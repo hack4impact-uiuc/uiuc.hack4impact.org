76fafc8778007870dce27444208327ad4d553fad
Arpan Laha <arpanlaha99@gmail.com>
2020-08-09 03:04:42 +0000

    switch to prettier 2

--- data/ngoApplyData.js
+++ data/ngoApplyData.js
@@ -5,13 +5,12 @@ import ActionButton from "../components/actionButton";
 import Link from "next/link";
 
 // constants that will affect links in the nonprofit application page
-const applicationLink =
-  "https://forms.gle/jaEsJHkBtzn9M5jt7";
+const applicationLink = "https://forms.gle/jaEsJHkBtzn9M5jt7";
 const interestFormLink =
   "https://docs.google.com/forms/u/1/d/e/1FAIpQLSeart7T-6sR61MXhdOfsN0n7oJ8ZThYeNjdqPnsLQLzLNKL2A/viewform";
 const projectAppExample =
   "https://docs.google.com/document/d/1SGG7gxu8lgWMOhxIULMSUZQa-4p0bLwrwDl4SAZW2lE/edit";
-const pitchbookLink = "https://h4i-pitchbook.now.sh/"
+const pitchbookLink = "https://h4i-pitchbook.now.sh/";
 
 // constants for dates
 const applicationDueDate = "September 1, 2019";
@@ -54,31 +53,33 @@ export default {
       title: "Initial Contact",
       detail: (
         <>
-          Reach out to our team via <a href="mailto:uiuc@hack4impact.org">email</a> to 
-          get started with the application process. We'd love to hear more about your organization 
-          and potential project ideas as it will help us get an initial sense of your organization's goals, 
-          needs, and expectations while you progress through the application process.
+          Reach out to our team via{" "}
+          <a href="mailto:uiuc@hack4impact.org">email</a> to get started with
+          the application process. We'd love to hear more about your
+          organization and potential project ideas as it will help us get an
+          initial sense of your organization's goals, needs, and expectations
+          while you progress through the application process.
         </>
       ),
-      icon: <Contract />
+      icon: <Contract />,
     },
     {
       title: "Context",
       detail:
         "We will reach out shortly to schedule a call to dive deeper into your organization and project ideas, while also explaining more about what we do and our goals. Then, we will also work with you to define and clarify your project ideas.",
-      icon: <Phone />
+      icon: <Phone />,
     },
     {
       title: "Scoping",
       detail:
         "We may schedule additional calls to further tighten your project specifications. With these additional meetings, we aim to dive deep into your specific ideas to gain a better understanding of the reasoning, value, and impact of these potential projects, outline specifications for these ideas, and narrow them down to a few that we believe we can reasonably accomplish with our development timeline and that align with your organization and our goals.",
-      icon: <Phone />
+      icon: <Phone />,
     },
     {
       title: "Ideation",
       detail:
         "We aim to create the most impactful product for your organization and ours. Our team will continue to work with your organization to gather additional insight through discovering pain points, conducting user research, and incorporating other feedback from your organization. This way, our development teams can jump right in after ideation and build the best product that serves your needs!",
-      icon: <Contract />
+      icon: <Contract />,
     },
     {
       title: "Project Application Due",
@@ -96,14 +97,14 @@ export default {
       ),
       icon: <Contract />,
       //date: applicationDueDate
-    }
+    },
   ],
   quote: {
     text:
       "We absolutely recommend working with Hack4Impact. We needed to address English competency in Cambodia, and Hack4Impact was the actual organization who helped us get it done. Hack4Impact gives nonprofit organizations an opportunity to collaborate and support their mission in ways they even may not have considered.",
     source: "Kelly Wolfe",
     sourceTitle:
-      "Love Without Boundaries Managing Director of Strategic Partnerships"
+      "Love Without Boundaries Managing Director of Strategic Partnerships",
   },
   faq: [
     {
@@ -123,29 +124,32 @@ export default {
           happen around the middle of the semester and the handoff, which will
           be around the end of the semester. For more information, look{" "}
           <a href="https://h4i-project-timeline-qonwikgcqk.now.sh">here.</a>
-          <br/>
-          <br/>
-          Traditionally, projects and development will span one semester, after which
-          our team will transfer ownership of the repository and any additional resources 
-          created during the development process. More recently, however, some projects have 
-          spanned the course of an entire year in an effort to ensure quality and impact. 
-          The development process follows the exact same structure in these cases, where a 
-          fully functional prototype is completed after one semester. However, these projects 
-          continue by gathering feedback during the next semester and iterating on the 
-          prototype to ensure that we're creating the most impactful and useful product for our 
-          clients. The length of the project is discussed and agreed upon from both parties before official development.
+          <br />
+          <br />
+          Traditionally, projects and development will span one semester, after
+          which our team will transfer ownership of the repository and any
+          additional resources created during the development process. More
+          recently, however, some projects have spanned the course of an entire
+          year in an effort to ensure quality and impact. The development
+          process follows the exact same structure in these cases, where a fully
+          functional prototype is completed after one semester. However, these
+          projects continue by gathering feedback during the next semester and
+          iterating on the prototype to ensure that we're creating the most
+          impactful and useful product for our clients. The length of the
+          project is discussed and agreed upon from both parties before official
+          development.
         </>
-      )
+      ),
     },
     {
       question: "How often do teams meet with their clients?",
       answer:
-        "Meeting frequency is very dependent on the team and client. Generally, teams hold weekly or biweekly meetings with their clients. Other checkpoints include initial calls to scope out and define the project with our project leads, an MVP demo, and a Final Handoff presentation."
+        "Meeting frequency is very dependent on the team and client. Generally, teams hold weekly or biweekly meetings with their clients. Other checkpoints include initial calls to scope out and define the project with our project leads, an MVP demo, and a Final Handoff presentation.",
     },
     {
       question: "Is there a development fee?",
       answer:
-        "We typically charge a fee of $1,000 per semester to our clients for any work that we do. As a student-led nonprofit building products for other nonprofits, we typically incur costs from using services like Amazon AWS or other third-party APIs as we build our products. We also aim to develop our members professionally and foster a collaborative, passionate, and dedicated community by offering tech talks, networking events, and organization-wide retreats. Thus, the development fee mainly goes towards technology costs, professional development, and community events. However, we understand that nonprofits may have different financial circumstances and are open to making accommodations on a case by case basis."
+        "We typically charge a fee of $1,000 per semester to our clients for any work that we do. As a student-led nonprofit building products for other nonprofits, we typically incur costs from using services like Amazon AWS or other third-party APIs as we build our products. We also aim to develop our members professionally and foster a collaborative, passionate, and dedicated community by offering tech talks, networking events, and organization-wide retreats. Thus, the development fee mainly goes towards technology costs, professional development, and community events. However, we understand that nonprofits may have different financial circumstances and are open to making accommodations on a case by case basis.",
     },
     {
       question: "How can I maximize my chances of working with Hack4Impact?",
@@ -158,7 +162,7 @@ export default {
           detailed application would be extremely helpful!{" "}
           <a href={projectAppExample}>Sample Application</a>
         </>
-      )
+      ),
     },
     {
       question: "What are some examples of projects?",
@@ -189,13 +193,14 @@ export default {
             </li>
           </ul>
         </>
-      )
-    }
+      ),
+    },
   ],
   applicationDetail: (
     <>
       Our application process is designed to be as straightforward as possible.
-      As long as you fill out an application, {/*before <b>{applicationDueDate}</b>*/}
+      As long as you fill out an application,{" "}
+      {/*before <b>{applicationDueDate}</b>*/}
       we will carefully consider your submission and reach out to you if our
       goals align. Some aspects of the application we pay close attention to
       include technical complexity, impact, and mission. Our application link is{" "}
@@ -210,5 +215,5 @@ export default {
       and we’ll still love to talk to you and potentially work with your
       organization in {nextSemester}.
     </b>
-  )
+  ),
 };
